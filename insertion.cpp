1e372d724459f9a2ff96a7e9314a872025e319e8
Ishan Arora <141183298+Ishanarora35@users.noreply.github.com>
2026-09-13 12:43:32 -0700

    linked list insertion code, I DID IT ALL ALONE RAHHHH WE UNDERSTAND THAT SHIT NOW

---
+++ insertion.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+struct node {
+    int data = 0;
+    node* next = nullptr;
+};
+
+
+void print(node* current) {
+    for (node* temp = current; temp; temp = temp->next) {
+        cout << temp->data << endl;
+    }
+}
+
+void add(node* &current, int x) {
+    node* added = new node;
+    added->data = x;
+    node* temp = current;
+    if (current == nullptr) {
+        added = current;
+    }
+    else {
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = added;
+    }
+}
+
+int main() {
+
+    node* head = new node;
+    head->data = 5;
+    node* second = new node;
+    second->data = 10;
+    head->next = second;
+    print(head);
+    cout << "now trying out the insertion" << endl;
+    add(second, 15);
+    print(head);
+    cout << "lets keep going pal" << endl;
+    add(second, 20);
+    print(head);
+
+}
